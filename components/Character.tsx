f470d9d33ca3dec5ab713c72868a6cf05ee2dd4d
Anton Palermo <ANPBusinessmail@gmail.com>
2022-09-23 02:44:30 +0800

    initialize tailwind

--- components/Character.tsx
+++ components/Character.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { CharacterSchema } from '../lib/schema/character'
 
 export type CharacterProps = {
@@ -7,7 +8,13 @@ export type CharacterProps = {
 export default function Character({ character }: CharacterProps) {
   return (
     <div>
-      <h1>{character.name}</h1>
+      <h1 className="font-medium">{character.name}</h1>
+      <Image
+        src={character.image}
+        alt={`Photograph of ${character.name}`}
+        width={100}
+        height={100}
+      />
     </div>
   )
 }
