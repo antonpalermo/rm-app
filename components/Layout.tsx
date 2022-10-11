70f07324d4dd363904f1544084da914f300c64d2
Anton Palermo <ANPBusinessmail@gmail.com>
2022-10-11 22:57:55 +0800

    fix: nav styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -18,9 +18,9 @@ export function Layout({ title, children }: LayoutProps) {
       </Head>
       <header className="bg-white shadow">
         <div className="container mx-auto px-5">
-          <div className="relative w-full py-3 inline-flex justify-end sm:justify-between items-center">
+          <div className="relative w-full py-3 inline-flex justify-between items-center">
             <Link href={'/'} passHref>
-              <a className="sm:text-center sm:absolute sm:left-[50%] sm:-translate-x-1/2 font-semibold text-gray-900 hover:text-blue-500">
+              <a className="font-semibold text-gray-900 hover:text-blue-500">
                 Rick and Morty
               </a>
             </Link>
@@ -28,7 +28,7 @@ export function Layout({ title, children }: LayoutProps) {
           </div>
         </div>
       </header>
-      <div className="sm:w-9/12 mx-auto my-16 px-5">{children}</div>
+      <div className="container mx-auto my-16 px-5">{children}</div>
       <Footer />
     </>
   )
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 import { Link, Button } from '@components'
 import { Bars3Icon } from '@heroicons/react/20/solid'
-
-export type NavbarProps = {}
+import { Transition } from '@headlessui/react'
 
 function MobileNavbar({
   paths,
@@ -16,13 +15,32 @@ function MobileNavbar({
 }) {
   return (
     <>
-      {toggle && <h1>toggled</h1>}
       <Button onClick={onToggle} className="block sm:hidden">
         <Bars3Icon className="h-5 w-5" />
       </Button>
+      <Transition
+        show={toggle}
+        enter="transition-opacity ease-in-out duration-200"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity ease-in-out duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="fixed top-[60px] left-0 right-0 bottom-0 bg-black bg-opacity-20" />
+        <div className="w-full sm:hidden absolute bg-white inset-x-0 -bottom-36 px-5 py-5 shadow rounded-md">
+          {paths.map((p, i) => (
+            <Link key={p.id + i} href={p.path} className="text-center">
+              {p.label}
+            </Link>
+          ))}
+        </div>
+      </Transition>
     </>
   )
 }
+
+export type NavbarProps = {}
 
 export function Navbar({}: NavbarProps) {
   const [toggle, setToggle] = React.useState<boolean>(false)
