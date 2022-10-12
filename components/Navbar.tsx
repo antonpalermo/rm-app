4b6d034049bb54ec8eb7dce9f23157540fa69935
Anton Palermo <ANPBusinessmail@gmail.com>
2022-10-12 18:34:25 +0800

    nav changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,17 +5,23 @@ import { Bars3Icon } from '@heroicons/react/20/solid'
 import { Transition } from '@headlessui/react'
 
 function MobileNavbar({
-  paths,
-  toggle,
-  onToggle
+  paths
 }: {
-  toggle: boolean
-  onToggle: () => void
   paths: { id: string; label: string; path: string }[]
 }) {
+  const [toggle, setToggle] = React.useState<boolean>(false)
+
+  function onCloseNav() {
+    setToggle(prevState => !prevState)
+  }
+
   return (
     <>
-      <Button onClick={onToggle} className="block sm:hidden">
+      <Button
+        onClick={onCloseNav}
+        onBlur={onCloseNav}
+        className="block sm:hidden"
+      >
         <Bars3Icon className="h-5 w-5" />
       </Button>
       <Transition
@@ -27,8 +33,8 @@ function MobileNavbar({
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
       >
-        <div className="fixed top-[60px] left-0 right-0 bottom-0 bg-black bg-opacity-20" />
-        <div className="w-full sm:hidden absolute bg-white inset-x-0 -bottom-36 px-5 py-5 shadow rounded-md">
+        <div className="fixed sm:hidden top-[60px] left-0 right-0 bottom-0 bg-black bg-opacity-20 z-40" />
+        <div className="w-full sm:hidden absolute bg-white inset-x-0 -bottom-36 px-5 py-5 shadow rounded-md z-50">
           {paths.map((p, i) => (
             <Link key={p.id + i} href={p.path} className="text-center">
               {p.label}
@@ -43,15 +49,10 @@ function MobileNavbar({
 export type NavbarProps = {}
 
 export function Navbar({}: NavbarProps) {
-  const [toggle, setToggle] = React.useState<boolean>(false)
   const paths = [
     { id: '0x1', label: 'Characters', path: '/characters' },
     { id: '0x2', label: 'Locations', path: '/locations' }
   ]
-
-  function toggleNav() {
-    setToggle(prev => !prev)
-  }
 
   return (
     <nav>
@@ -62,7 +63,7 @@ export function Navbar({}: NavbarProps) {
           </Link>
         ))}
       </div>
-      <MobileNavbar toggle={toggle} onToggle={toggleNav} paths={paths} />
+      <MobileNavbar paths={paths} />
     </nav>
   )
 }
